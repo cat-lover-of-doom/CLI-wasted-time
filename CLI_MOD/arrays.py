16318b921324f73f245bd49c525f4d8d31f6b7ac
waitcat <ggsesbisgg@gmail.com>
2022-06-13 21:08:14 +0800

    amogos

--- CLI_MOD/arrays.py
+++ CLI_MOD/arrays.py
@@ -73,43 +73,57 @@ class _buffer:
     def set(self, x, y, char_tupple):
         self.double_buffer[y][x] = char_tupple
 
-    def set_string(self, string, x, y, fg, bg, attr):
+    def set_string(self, x, y, string, fg=15, bg=0, attr=1):
         if x + len(string) > self.width or y > self.height:
-            raise Exception
-
+            pass
         for index,  char in enumerate(string):
             self.set(x + index, y, (char, fg, bg, attr))
 
 
 class ball:
-    def __init__(self, height, width, buffer, pos=None, card=None) -> None:
+    def __init__(self, height, width, buffer, pos=None, card=None, text='0', fg=15, bg=0, attr=1) -> None:
         self.buffer = buffer
         self.field = (width, height)
         self.pos = pos if pos is not None else [0, 0]
         self.card = card if card is not None else [1, 1]
+        self.text = (text, fg, bg, attr)
 
     def main(self):
-        self.buffer.set(self.pos[0], self.pos[1], '0')
+        self.buffer.set_string(self.pos[0], self.pos[1], *self.text)
         self.pos[0] += self.card[0]
         self.pos[1] += self.card[1]
         self.check()
 
     def check(self):
         temp = []
+        count = 0
         for card, boundry, position in zip(self.card, self.field, self.pos):
+            if count == 0:
+                boundry -= len(self.text[0])
+
             if position >= boundry - abs(card) or position <= 0:
                 card = -card
+                self.colision()
+
             temp.append(card)
+            count += 1
+
         self.card = temp
+
+    def colision(self):
+        tup = self.text[:]
+        self.text = (tup[0], tup[2], tup[1], tup[3])
 
 
 buff = _buffer()
-b = ball(buff.height, buff.width, buff)
-b1 = ball(buff.height, buff.width, buff, pos=[100, 10], card=[1, -1])
+b1 = ball(buff.height, buff.width, buff, pos=[
+          10, 10], card=[1, -1], text='amogos')
 os.system('cls')
-while True:
-    b.main()
-    b1.main()
-    buff.flush()
-    buff.clear()
-    time.sleep(0.01)
+try:
+    while True:
+        b1.main()
+        buff.flush()
+        buff.clear()
+        time.sleep(0.03)
+finally:
+    print('\033[0m')
